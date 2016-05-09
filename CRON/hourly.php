cd6e1810d7ae88190e11204183ee1c3fd960cc35
Marvin Sanchez <sanchezmarvin38@gmail.com>
2016-05-09 17:09:54 +0200

    Finalisation CRON

--- CRON/hourly.php
+++ CRON/hourly.php
@@ -27,6 +27,8 @@ $aAccount = array(
     'whiteaddicted',
 );
 
+set_time_limit(0);
+
 foreach ($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC) as $row) {
     $instagram->setAccessToken($row['access_token']);
     $user = $instagram->getUser();
@@ -68,21 +70,21 @@ foreach ($pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC) as $row) {
         }
     } while (($follower = $instagram->pagination($follower)) && $i < $limit / 2);
 
+
     //Unfollow des personnes qui ne suivent pas et que l'on ne vient pas de suivre
     $i = $j = 0;
     $follower = $instagram->getUserFollower();
     do {
         while ($j < count($follower->data) && $i < $limit / 2) {
-            $_user = $instagram->getUser($follower->data[$j]->id);
-            if (! isset($aFollowing[$_user->data->id]) && ! isset($aFollow[$_user->data->id])) {
+            if (! isset($aFollowing[$follower->data[$j]->id]) && ! isset($aFollow[$follower->data[$j]->id])) {
                 $result = $instagram->modifyRelationship('unfollow', $follower->data[$j]->id);
                 if ($result->meta->code == 200) {
-                    $aFollow[$_user->data->id] = $user->data->id;
+                    $aFollow[$follower->data[$j]->id] = $follower->data[$j]->id;
                     echo 'unfollow ' . $i . '<br>';
                     $i++;
                 }
             }
             $j++;
         }
-    } while ($follower = $instagram->pagination($follower) || $i < $limit / 2);
+    } while (($follower = $instagram->pagination($follower)) && $i < $limit / 2);
 }
